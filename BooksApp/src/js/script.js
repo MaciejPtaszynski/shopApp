e4e5debd8aeef9ed1ef885d5dc04593937bb2ab4
Maciej Ptaszynski <mpsendit@gmail.com>
2022-03-30 19:43:24 +0200

    add first pst of initAction

--- BooksApp/src/js/script.js
+++ BooksApp/src/js/script.js
@@ -1,28 +1,46 @@
-const { render } = require("sass");
-
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 {
-//     'use strict';
-    // const select = {
-    //     templateOf: {
-    //     booksTemplate: '#template-book' 
-    //     }
-    // };
+  'use strict';
 
-    const template = {
-        books: Handlebars.compile(document.querySelector(select.templateOf.books).innerHTML),
-    };    
-    // eslint-disable-line no-unused-vars
+  const select = {
+    templateOf: {
+      books: '#template-book' 
+    },
+    list: {
+      booksList: '.books-list',
+      booksImage: '.book__image'
+    }
+  };
+
+  const template = {
+    books: Handlebars.compile(document.querySelector(select.templateOf.books).innerHTML),
+  };    
     
-        render()
-            const thisbooksList = this;
+  function render(){
 
-            for(let book of dataSource.books){
-                const generateHTML = template.booksTemplate(book);
-                const generateDOM = utils.createDOMFromHTML(generateHTML);
-                booksList.appendChild(generateDOM);
-            }
-        }
+    const booksList = document.querySelector(select.list.booksList);  
+
+    for(let book of dataSource.books){
+      const generateHTML = template.books(book);
+      const generateDOM = utils.createDOMFromHTML(generateHTML);
+      booksList.appendChild(generateDOM);
+    }
+            
+  }
+  render();
     
-} 
+  
 
+  function initAction(){
+    
+    const favoriteBooks = [];
+
+    const booksList = document.querySelector(select.list.booksImage);
+    booksList.addEventListener('dblclick', function(event){
+      event.preventDefault();
+      
+      booksList.classList.add('favorite');
+      
+    });
+  }
+}
