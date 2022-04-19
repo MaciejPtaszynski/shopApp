38e5fa048a9b0ced51ab9be3d464cc031433e903
Maciej Ptaszynski <mpsendit@gmail.com>
2022-04-19 21:23:02 +0200

    working app no components

---
+++ shop-app/src/index.js
@@ -0,0 +1,14 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import './styles/normalize.scss';
+import './styles/global.scss';
+import 'font-awesome/css/font-awesome.min.css';
+
+ReactDOM.render(
+  <StrictMode>
+  <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
